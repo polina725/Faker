280df09a737951916101dd0eb8813fca72bfb8d3
polina725 <62598798+polina725@users.noreply.github.com>
2020-10-09 23:22:27 +0300

    changed list generator

---
+++ Faker/GeneratorContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker
+{
+    public class GeneratorContext
+    {
+
+        public Random Random { get; }
+
+        public Type TargetType { get; }
+
+        public Faker Faker { get; }
+
+        public GeneratorContext(Random random, Type targetType, Faker faker)
+        {
+            Random = random;
+            TargetType = targetType;
+            Faker = faker;
+        }
+    }
+}
